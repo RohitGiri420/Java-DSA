c271af1692c6d58f1b4504ee7e2d8358af7a4e36
Rohit Giri <giri420rohit@gmail.com>
2024-04-20 12:45:27 +0530

    Reverse the num program is added

---
+++ Loops/ReverseNum.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class ReverseNum {
+
+    public static void main(String[] args) {
+        int num = 1234225;
+        int revNum;
+         while (num >0) {
+
+            System.out.print(revNum = num%10);
+            num = num/10;
+            
+         }
+    }
+    
+}
